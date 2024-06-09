b2d86a10bc9a41f9a5e5d186ae9eb40e0e3e2553
gartonot <gartonot62@gmail.com>
2024-06-09 20:24:32 +0300

    Добавил мидлвар для аунтефикации и сгруппировал роуты по мидлвару

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth')->group(function() {
+    // Products
+    Route::resource('/products', 'ProductController')->only([
+        'store', 'update', 'destroy'
+    ]);
+});
 
 Route::resource('/products', 'ProductController')->only([
-    'index', 'show', 'store', 'update', 'destroy'
+    'index', 'show'
 ]);
-
