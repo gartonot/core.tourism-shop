7227b1d2b46a1b195ee5346dea3d70b5139fabfa
gartonot <gartonot62@gmail.com>
2024-06-12 14:29:46 +0300

    Подправил ответ мидлвара авторизации на случай если он не прошёл

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,9 +6,9 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-   protected function authenticate($request, array $guards)
-   {
-       if($request->bearerToken() === config('apitoken')) return;
-       $this->unauthenticated($request, $guards);
-   }
+    protected function authenticate($request, array $guards)
+    {
+        if($request->bearerToken() === config('apitoken')) return;
+        abort(response()->json(['message' => 'Unauthenticated'], 401));
+    }
 }
