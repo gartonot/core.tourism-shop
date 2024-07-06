e809bda1110826d30148e6a9976f0ca755f93dc1
gartonot <gartonot62@gmail.com>
2024-07-06 13:19:12 +0300

    Отформатировали товары для фронта

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -54,7 +54,6 @@ class OrderController extends Controller
 
         return [
             'id' => $order->id,
-            'order_id' => $order->order_id,
             'product' => $formattedProduct,
             'count' => $order->count,
             'created_at' => $order->created_at,
@@ -64,7 +63,7 @@ class OrderController extends Controller
     public function groupOrderById($orders) {
         $formattedOrder = array();
         foreach($orders as $order) {
-            $formattedOrder[] = $this->formattedOrder($order);
+            $formattedOrder[$order['order_id']][] = $this->formattedOrder($order);
         }
 
         return $formattedOrder;
